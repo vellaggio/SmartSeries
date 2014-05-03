f916639d7d1e32e7c41ab73accc3f2638bcbafe8
vellaggio <mikl.tm_7@hotmail.com>
2014-05-03 07:30:18 +0200

    fake chat acabat pro numes funciona ferran

---
+++ app-client/js/chat.js
@@ -0,0 +1,44 @@
+
+
+//get params
+var path = $(location).attr('href');
+var spath=path.split("?");
+path=spath[1];
+var params = path.split("&");
+var i, id, values, name;
+for (i=0; i<params.length; i++){
+  if (params[i].indexOf("name")!=-1) { //id
+    values = params[i].split("=");
+    name= values[1].replace("%20", " ");
+    $(function(){
+      $('#chat_title').text(name);
+    });
+  }
+  if (params[i].indexOf("img")!=-1) { //id
+    values = params[i].split("=");
+    img= values[1];
+    $(function(){
+      $('.img_chat').attr("src", "img/"+img+"");
+    });
+  }
+}
+
+$(document).ready(function(){
+  $('#send_cont').click(function(){
+    $("#mesage_you").empty().text($('#input_send').val());
+    $("#mesage_you").parent().show();
+    $("#input_send").val("");
+  });
+});
+
+function getCName()
+{
+  return name;
+}
+
+function getCImg()
+{
+  return img;
+}
+
+
